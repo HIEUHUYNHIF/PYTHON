a532ccb45026a586745f36689e667880ba3f3d85
HIEU HUYNH <hieuhuynhif@gmail.com>
2022-12-02 14:07:06 +0700

    02/12/2022

--- Stock_index/main/test.py
+++ Stock_index/main/test.py
@@ -1,24 +1,7 @@
-import matplotlib.pyplot as plt
-import scipy.stats
-import re
-import csv
 import numpy as np
-
-data = []
-
-with open("../dataset/VNINDEX_from_01-07-2021_to_31-12-2021.csv") as file:
-    reader = csv.reader(file, delimiter=',')
-    for row in reader:
-
-        data.append(float(re.findall(r"[-+]?\d*\.\d+|\d+",row[6])[0]))
-
-# plt.hist(data, bins=100, density=True)
-# plt.show()
-
-dist = scipy.stats.norm
-
-bounds = [(-100,100), (-100,100)]
-res = scipy.stats.fit(dist, data,bounds)
-print(res)
-res.plot()
-plt.show()
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import math
+from sklearn.preprocessing import MinMaxScaler
